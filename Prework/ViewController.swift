5f57fa735affeace4f51583566e55816046c63c2
hannahphung <84505812+hannahphung@users.noreply.github.com>
2021-08-16 11:47:50 +0700

    Added slider view
    User can choose between basic tip percentages option or using slider to custom

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -7,31 +7,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
     @IBOutlet weak var billAmountTextField: UITextField!
     @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
     
+    @IBOutlet weak var Switch: UISwitch!
+    @IBOutlet weak var tipSlider: UISlider!
+    @IBOutlet weak var tipSliderlabel: UILabel!
+    @IBOutlet weak var sliderView: UIView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
+        billAmountTextField.becomeFirstResponder()
+    }
+    
+    
+    @IBAction func switchToSlider(_ sender: Any) {
+        if (Switch.isOn){
+            sliderView.isHidden = false
+        }
+        if (Switch.isOn == false){
+            sliderView.isHidden = true
+        }
+    }
+    
+    func updateLabel(_ tip: Double, _ total: Double){
+        // Update Tip Amount label
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        // Update Total label
+        totalLabel.text = String(format: "$%.2f", total)
     }
     
     @IBAction func calculateTip(_ sender: Any) {
-        // Get bill amount from text field input
+        // Get bill amount from text field inpute
         let bill = Double(billAmountTextField.text!) ?? 0
         
         // Get total tip by multiplying tip * tipPercentage
-        let tipPercentages = [0.15, 0.18, 0.2]
+        let tipPercentages = [0.15,0.18,0.2]
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
-        let total = bill + tip
+        let total = tip + bill
         
-        // Update Tip Amount Label
-        tipAmountLabel.text = String(format: "$%.2f", tip)
-        // Update Total Amount
-        totalLabel.text = String(format: "$%.2f", total)
+        updateLabel(tip, total)
+    }
+    
+    @IBAction func calculateTipSlider(_ sender: Any) {
+        tipSliderlabel.text = String(format: "%.2f", tipSlider.value*100) + "%"
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentage = Double(tipSlider.value)
+        let tip = bill * tipPercentage
+        let total = tip + bill
+        
+        updateLabel(tip, total)
     }
 }
 
